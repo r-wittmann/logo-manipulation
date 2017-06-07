74489b8c25b170135ed3266286a0c3f47865fe51
Rainer Wittmann <rainer.wittmann@comsysto.com>
2017-06-07 14:23:06 +0200

    add .gitignore and improve manipulation code

--- logo-manipulation.js
+++ logo-manipulation.js
@@ -1,10 +1,21 @@
-var Jimp = require("jimp");
+var Jimp = require('jimp')
+var fs = require('fs')
 
-// open a file called "lenna.png"
-Jimp.read("original_logos/dachser.jpg", function (err, dachser) {
-    if (err) throw err;
-    dachser.resize(256, 256)            // resize
-         .quality(60)                 // set JPEG quality
-         .greyscale()                 // set greyscale
-         .write("manipulated_logos/dachser/smallbw.jpg"); // save
-});
+var manipulateLogos = (filename) => {
+  Jimp.read(`original_logos/${filename}`, (err, image) => {
+    err && console.error(err)
+    image.resize(256, 256)
+      .write(`manipulated_logos/${filename.split('.')[0]}/small.png`)
+  })
+}
+
+var readFiles = (dirname) => {
+  fs.readdir(dirname, (err, filenames) => {
+    err && console.error(err)
+    filenames.forEach((filename) => {
+      manipulateLogos(filename)
+    })
+  })
+}
+
+readFiles('original_logos/')
